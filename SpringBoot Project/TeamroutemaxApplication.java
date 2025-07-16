91eb0f969d9fb0224966ef827abea28fb072dafa
23WH1A05I3-Vaidehi <154442546+23WH1A05I3-Vaidehi@users.noreply.github.com>
2025-07-16 15:21:06 +0530

    Create TeamroutemaxApplication.java

---
+++ SpringBoot Project/TeamroutemaxApplication.java
@@ -0,0 +1,13 @@
+package com.routemax.teamroutemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamroutemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamroutemaxApplication.class, args);
+	}
+
+}
