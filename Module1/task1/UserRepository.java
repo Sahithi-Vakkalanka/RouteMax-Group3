aef9241ec935e6268375734301dac9cd9abc3b9f
Supragna-2705 <supragnakonakala@gmail.com>
2025-07-21 20:46:13 +0530

    Cleanup unused files

--- Module1/task1/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.routemax.teamroutemax.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.routemax.teamroutemax.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
